12f228a2599d91ceab285e0a18f21a9d6040edb7
qlfflwls5 <qlfflwls2@gmail.com>
2021-01-26 22:14:50 +0900

    2021-01-26 TIL

---
+++ 2021-01-26/D1_8_SWEA_2027.py
@@ -0,0 +1,30 @@
+# 주어진 텍스트를 그대로 출력하세요.
+
+#   #++++
+#   +#+++
+#   ++#++
+#   +++#+
+#   ++++#
+
+
+# '+' 0개 + '#' 1개 + '+' 4개,
+# '+' 1개 + '#' 1개 + '+' 3개 ...의 규칙이다.
+for i in range(5):
+    print('+' * i + '#' + '+' * (4 - i))
+
+
+#2 .insert()를 사용
+for i in range(5):
+    n = ['+', '+', '+', '+']
+    n.insert(i, '#')
+    print(''.join(n))
+
+
+#3 index를 활용
+for i in range(5):
+    for j in range(5):
+        if i == j:
+            print('#', end='')
+        else:
+            print('+', end='')
+    print()
