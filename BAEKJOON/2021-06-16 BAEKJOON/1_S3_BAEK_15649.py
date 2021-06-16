9033fb3074c2362a68e0e5771b30d5f5f141eb37
qlfflwls5 <qlfflwls2@gmail.com>
2021-06-16 23:57:07 +0900

    2021-06-16 TIL 15649, 1874 (S3)

--- BAEKJOON/2021-06-16 BAEKJOON/1_S3_BAEK_15649.py
+++ BAEKJOON/2021-06-16 BAEKJOON/1_S3_BAEK_15649.py
@@ -10,3 +10,22 @@
 # 출력
 # 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
 # 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+
+def dfs(level):
+    if level == M:
+        print(*sel)
+        return
+
+    for i in range(1, N + 1):
+        if not used[i]:
+            sel[level] = i
+            used[i] = 1
+            dfs(level + 1)
+            used[i] = 0
+
+
+N, M = map(int, input().split())
+used = [0]*(N+1)
+sel = [0]*M
+dfs(0)
